19a97d7e8875e1a9455cd538c2d5ae73ed1a8d45
Garry Dmello <garrynigel@gmail.com>
2016-05-02 19:29:42 -0700

    Compiled Version 2

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,6 @@ public class Main {
 	    AVPlayer.loadAudio(args[1]);
 	    AVPlayer.loadVideo(args[0]);
 	    AVPlayer.initialize();
-
-
-
-
-
-        /*
-        PlayerComponents AVPlayer = new PlayerComponents("/home/sailesh/Documents/summarized.rgb",  "/home/sailesh/Documents/summarized.wav");
-		AVPlayer.loadAudio();
-		AVPlayer.loadVideo();
-		AVPlayer.initialize();*/
-
-		//VideoSearch search = new VideoSearch(args[0],args[2]);
     }
 
 }
